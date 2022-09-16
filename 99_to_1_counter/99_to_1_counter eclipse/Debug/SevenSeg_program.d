75b886f4164b64c74622599ebe029cab90965054
Omar Nagdi <105320541+OmarNagdi@users.noreply.github.com>
2022-09-16 03:17:01 +0200

    Add files via upload

---
+++ 99_to_1_counter/99_to_1_counter eclipse/Debug/SevenSeg_program.d
@@ -0,0 +1,10 @@
+SevenSeg_program.o SevenSeg_program.o: ../SevenSeg_program.c \
+  ../STD_TYPES.h ../BIT_MATH.h ../DIO_Interface.h ../SevenSeg_Interface.h
+
+../STD_TYPES.h:
+
+../BIT_MATH.h:
+
+../DIO_Interface.h:
+
+../SevenSeg_Interface.h:
